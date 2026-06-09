d967835ebe42a4365ef556659aecf321f593eee3
DwijeshAwasthi <dwijeshawasthiawasthi@gmail.com>
2026-06-10 02:40:12 +0530

    complete bronze layer ingestion pipeline

--- scripts/ingest_bronze.py
+++ scripts/ingest_bronze.py
@@ -1,11 +1,15 @@
 import os
+import sys
 from dotenv import load_dotenv
+import warnings
+import json
 import requests
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import current_timestamp, lit
 
 load_dotenv()
-print(os.getenv("URL"))
+os.environ['PYSPARK_PYTHON'] = sys.executable
+os.environ['PYSPARK_DRIVER_PYTHON'] = sys.executable
 
 def fetch_data():
     url=os.getenv("URL")
@@ -20,8 +24,46 @@ def fetch_data():
         print(f"Error code{response.status_code}")
         return []
 
+def main():
+    warnings.filterwarnings("ignore", category=UserWarning)
+    warnings.filterwarnings("ignore", category=DeprecationWarning)
+    raw_jobs=fetch_data()
+
+    if not raw_jobs:
+        print("No data to process. Existing")
+        return
+    spark=SparkSession.builder\
+    .appName("JobMarketBronzeIngestion") \
+    .getOrCreate()
+    print("Spark session is active")
+    spark.sparkContext.setLogLevel("ERROR")
+
+
+    df=spark.createDataFrame(raw_jobs)
+    bronze_df=df.withColumn("ingested_at", current_timestamp())\
+    .withColumn("data_source", lit("arbeitnow_api"))
+
+    print("Schema for bronze layer:")
+    bronze_df.printSchema()
+
+    print("\n Preview: ")
+    local_rows = [row.asDict() for row in bronze_df.collect()]
+    from datetime import datetime
+    timestamp_str = datetime.now().strftime("%Y%m%d_%H%M%S")
+    output_file_path = f"data/bronze/job_postings/batch_{timestamp_str}.json"
+    os.makedirs("data/bronze/job_postings", exist_ok=True)
+    with open(output_file_path, "w", encoding="utf-8") as f:
+        for row in local_rows:
+            if 'ingested_at' in row and row['ingested_at'] is not None:
+                row['ingested_at']= row['ingested_at'].isoformat()
+            f.write(json.dumps(row)+ "\n")
+
+    with open("data/bronze/job_postings/_SUCCESS", "w") as f:
+        f.write("")
+
+    
+    print("Raw data landed in bronze layers successfullt")
+    spark.stop()        
+
 if __name__=="__main__":
-    jobs=fetch_data()
-    if jobs:
-        print("\n\n Sample Job Structure:")
-        print(jobs[0])
+    main()
